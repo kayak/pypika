20f008747401d575be1284bc4a041ed0730ee1ec
Timothy Heys <theys@kayak.com>
2016-07-07 18:36:19 +0200

    Renaming docs/README.rst to docs/index.rst and adding a symlink README.rst in the root

--- docs/conf.py
+++ docs/conf.py
@@ -50,7 +50,7 @@ source_suffix = '.rst'
 # source_encoding = 'utf-8-sig'
 
 # The master toctree document.
-master_doc = 'README'
+master_doc = 'index'
 
 # General information about the project.
 project = 'pyqb'
