4e143e04cacfb5afa4afc6e9e67f6702e412f53f
Timothy Heys <theys@kayak.com>
2016-09-20 15:01:06 +0200

    Bumped version to 0.0.32

--- pypika/__init__.py
+++ pypika/__init__.py
@@ -39,4 +39,4 @@ from .utils import JoinException, GroupingException, CaseException, UnionExcepti
 
 __author__ = "Timothy Heys"
 __email__ = "theys@kayak.com"
-__version__ = "0.0.30"
+__version__ = "0.0.32"
