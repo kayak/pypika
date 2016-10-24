0c0a94374deb529ad1107074a8f0d3d8421f8d36
Timothy Heys <theys@kayak.com>
2016-10-24 16:39:36 +0200

    Bumped version to 0.1.0

--- pypika/__init__.py
+++ pypika/__init__.py
@@ -31,12 +31,6 @@ pypika.utils
 This contains all of the utility classes such as exceptions and decorators.
 
 """
-
-from .enums import Order, JoinType, DatePart
-from .queries import Query, Table, make_tables as Tables
-from .terms import Field, Case, Interval, Rollup
-from .utils import JoinException, GroupingException, CaseException, UnionException, RollupException
-
 __author__ = "Timothy Heys"
 __email__ = "theys@kayak.com"
-__version__ = "0.0.32"
+__version__ = "0.1.0"
