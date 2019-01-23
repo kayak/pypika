8aada97d87d2bfaeacd63d1e4ed3199e8cd2c154
Timothy Heys <theys@kayak.com>
2019-01-23 16:32:20 +0100

    fixed valuewrapper to include its alias when present

--- pypika/tests/test_functions.py
+++ pypika/tests/test_functions.py
@@ -174,12 +174,22 @@ class ArithmeticTests(unittest.TestCase):
         self.assertEqual('SELECT (\"a\"+1)*(\"b\"-5) FROM \"abc\"', str(q1))
         self.assertEqual('SELECT (\"a\"+1)*(\"b\"-5) FROM \"abc\"', str(q2))
 
+    def test__complex_op_mul_no_parentheses(self):
+        q = Q.from_('abc').select(F('a') + 1 * F('b') - 5)
+
+        self.assertEqual('SELECT \"a\"+1*\"b\"-5 FROM \"abc\"', str(q))
+
     def test__complex_op_div_parentheses(self):
         q1 = Q.from_('abc').select((F('a') + 1) / (F('b') - 5))
         q2 = Q.from_(self.t).select((self.t.a + 1) / (self.t.b - 5))
 
         self.assertEqual('SELECT (\"a\"+1)/(\"b\"-5) FROM \"abc\"', str(q1))
         self.assertEqual('SELECT (\"a\"+1)/(\"b\"-5) FROM \"abc\"', str(q2))
+
+    def test__complex_op_div_no_parentheses(self):
+        q = Q.from_('abc').select(F('a') + 1 / F('b') - 5)
+
+        self.assertEqual('SELECT \"a\"+1/\"b\"-5 FROM \"abc\"', str(q))
 
     def test__arithmetic_equality(self):
         q1 = Q.from_('abc').select(F('a') / 2 == 2)
