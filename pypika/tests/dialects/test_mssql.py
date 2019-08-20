fc8ceb1c993331e8b024f68a88c44e7e8b972f8d
Paul Koenig <pmkoenig@gmail.com>
2019-08-20 08:14:05 -0700

    Support simple SQL Server TOP clauses.
    WITH TIES and PERCENT are not supported. Unit tests have been added
    for MSSQLQueryBuild select SQL generation.

---
+++ pypika/tests/dialects/test_mssql.py
@@ -0,0 +1,34 @@
+import unittest
+
+from pypika import Table
+from pypika.dialects import MSSQLQuery
+from pypika.utils import QueryException
+
+
+class SelectTests(unittest.TestCase):
+    table_abc = Table('abc')
+
+    def test_normal_select(self):
+        q = MSSQLQuery.from_('abc').select('def')
+
+        self.assertEqual('SELECT "def" FROM "abc"', str(q))
+
+    def test_distinct_select(self):
+        q = MSSQLQuery.from_('abc').select('def').distinct()
+
+        self.assertEqual('SELECT DISTINCT "def" FROM "abc"', str(q))
+
+    def test_top_distinct_select(self):
+        q = MSSQLQuery.from_('abc').select('def').top(10).distinct()
+
+        self.assertEqual('SELECT TOP (10) DISTINCT "def" FROM "abc"', str(q))
+
+    def test_top_select(self):
+        q = MSSQLQuery.from_('abc').select('def').top(10)
+
+        self.assertEqual('SELECT TOP (10) "def" FROM "abc"', str(q))
+
+    def test_top_select_non_int(self):
+        with self.assertRaisesRegex(QueryException,
+                                    'TOP value must be an integer'):
+            MSSQLQuery.from_('abc').select('def').top('a')
