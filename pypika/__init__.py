ac26a63cdfe2121ca371d4cc6578395484ed366f
Glenn De Jonghe <gl3nn@users.noreply.github.com>
2020-10-28 14:24:27 +0100

    Bump version: 0.42.1 → 0.43.0

--- pypika/__init__.py
+++ pypika/__init__.py
@@ -100,7 +100,7 @@ from pypika.utils import (
 
 __author__ = "Timothy Heys"
 __email__ = "theys@kayak.com"
-__version__ = "0.42.1"
+__version__ = "0.43.0"
 
 NULL = NullValue()
 SYSTEM_TIME = SystemTimeValue()
