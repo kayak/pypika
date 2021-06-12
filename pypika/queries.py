76dd56551972715c1eb961f769c8118ee7eceb42
Alexander Shadchin <alexandr.shadchin@gmail.com>
2021-06-12 17:21:18 +0300

    Drop unnecessary .format()

--- pypika/queries.py
+++ pypika/queries.py
@@ -1538,7 +1538,7 @@ class Joiner:
         if not fields:
             raise JoinException(
                 "Parameter 'fields' is required when joining with "
-                "a using clause but was not supplied.".format(type=self.type_label)
+                "a using clause but was not supplied."
             )
 
         self.query.do_join(JoinUsing(self.item, self.how, [Field(field) for field in fields]))
