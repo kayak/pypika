63fa83afeaaee8ab7e9ffb211ca42d9b30731ac5
Alexander Shadchin <alexandr.shadchin@gmail.com>
2021-06-15 14:09:48 +0300

    Fix black .v2

--- pypika/queries.py
+++ pypika/queries.py
@@ -1536,9 +1536,7 @@ class Joiner:
 
     def using(self, *fields: Any) -> QueryBuilder:
         if not fields:
-            raise JoinException(
-                "Parameter 'fields' is required when joining with a using clause but was not supplied."
-            )
+            raise JoinException("Parameter 'fields' is required when joining with a using clause but was not supplied.")
 
         self.query.do_join(JoinUsing(self.item, self.how, [Field(field) for field in fields]))
         return self.query
