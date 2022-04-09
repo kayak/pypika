03a7e6fc38f9ba177bb7146791a5a81dedd2ea14
cattybaby723 <cattiger723@gmail.com>
2022-04-09 11:33:24 +0800

    Fix set with_namespace as False forcibly _set_sql() #675

--- pypika/queries.py
+++ pypika/queries.py
@@ -1530,7 +1530,7 @@ class QueryBuilder(Selectable, Term):
         return " SET {set}".format(
             set=",".join(
                 "{field}={value}".format(
-                    field=field.get_sql(**dict(kwargs, with_namespace=False)), value=value.get_sql(**kwargs)
+                    field=field.get_sql(**kwargs), value=value.get_sql(**kwargs)
                 )
                 for field, value in self._updates
             )
